ad22385f0d1b473d26a731a449e91ea558394a35
hashhashgo <79754126+hashhashgo@users.noreply.github.com>
2025-12-17 16:02:23 +0800

    threading local DB connection

--- finmcp/databases/__init__.py
+++ finmcp/databases/__init__.py
@@ -1,6 +1,11 @@
 from .base import BaseDB
 from typing import Dict
+import threading
 
-DB_CONNECTIONS: Dict[str, BaseDB] = {}
+_tls = threading.local()
+if not hasattr(_tls, "db_connections"):
+    _tls.db_connections = {}
+
+DB_CONNECTIONS: Dict[str, BaseDB] = _tls.db_connections
 
 __all__ = ["BaseDB", "DB_CONNECTIONS"]
