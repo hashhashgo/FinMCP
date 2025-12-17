3a75760b6bc76ded7d35afd22c9ade57e3a81b90
hashhashgo <79754126+hashhashgo@users.noreply.github.com>
2025-12-17 16:12:33 +0800

    thread local DB connections

--- finmcp/databases/__init__.py
+++ finmcp/databases/__init__.py
@@ -1,11 +1,29 @@
 from .base import BaseDB
 from typing import Dict
-import threading
+from contextvars import ContextVar
 
-_tls = threading.local()
-if not hasattr(_tls, "db_connections"):
-    _tls.db_connections = {}
+_db_connections: ContextVar[Dict[str, BaseDB]] = ContextVar(
+    "db_connections", default={}
+)
 
-DB_CONNECTIONS: Dict[str, BaseDB] = _tls.db_connections
+class ContextDBConnections:
+    def _get(self):
+        conns = _db_connections.get()
+        if conns is None:
+            conns = {}
+            _db_connections.set(conns)
+        return conns
+
+    def __getitem__(self, key):
+        return self._get()[key]
+
+    def __setitem__(self, key, value):
+        self._get()[key] = value
+
+    def __contains__(self, key):
+        return key in self._get()
+
+
+DB_CONNECTIONS = ContextDBConnections()
 
 __all__ = ["BaseDB", "DB_CONNECTIONS"]
