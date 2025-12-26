488738881b1825f6aac26f5447c03a374327ab42
hashhashgo <79754126+hashhashgo@users.noreply.github.com>
2025-12-26 17:15:20 +0800

    move _parse_datatime to finmcp.utils

--- finmcp/data_sources/__init__.py
+++ finmcp/data_sources/__init__.py
@@ -3,6 +3,7 @@ from typing import Optional, Union
 from enum import Enum
 from datetime import datetime, date, timedelta, timezone
 
+from finmcp.utils import _parse_datetime
 
 class DataType(Enum):
     STOCK = 'stock'
@@ -39,25 +40,7 @@ class DataSource(ABC):
 
 
     def _parse_datetime(self, datetime_input: Union[str, datetime, date, int]) -> datetime:
-        datetime_output = None
-        if isinstance(datetime_input, datetime):
-            datetime_output = datetime_input
-        elif isinstance(datetime_input, date):
-            datetime_output = datetime(datetime_input.year, datetime_input.month, datetime_input.day)
-        elif isinstance(datetime_input, int):
-            if datetime_input > 9999999999: datetime_input = datetime_input // 1000
-            datetime_output = datetime.fromtimestamp(datetime_input, tz=timezone.utc)
-        elif isinstance(datetime_input, str):
-            for fmt in ("%Y-%m-%d %H:%M:%S", "%Y/%m/%d %H:%M:%S", "%Y%m%d%H%M%S", "%Y-%m-%d", "%Y/%m/%d", "%Y%m%d"):
-                try:
-                    datetime_output = datetime.strptime(datetime_input, fmt)
-                    break
-                except ValueError:
-                    continue
-        else:
-            raise TypeError(f"Unsupported datetime input type: {type(datetime_input)}")
-        if not isinstance(datetime_output, datetime): raise ValueError(f"String datetime format not recognized: {datetime_input}")
-        return datetime_output.astimezone()
+        return _parse_datetime(datetime_input)
     
     def _parse_date(self, date_input: Union[str, datetime, date, int]) -> date:
         dt = self._parse_datetime(date_input)
--- finmcp/databases/utils.py
+++ finmcp/databases/utils.py
@@ -5,6 +5,7 @@ from hashlib import sha1
 from typing import Dict, Optional, Any
 from datetime import datetime, date, timezone
 
+from finmcp.utils import _parse_datetime
 
 def _python_type_to_sqlite_type(py_type: str) -> str:
     if py_type in ["int", "bool"]:
@@ -91,27 +92,6 @@ def _sqlite_value_to_pandas_value(df: pd.DataFrame, type_dict: Dict[str, str]) -
             df[col] = df[col].astype(bool)
         cols.append(col)
     return df[cols]
-
-def _parse_datetime(datetime_input: str | datetime | date | int) -> datetime:
-    datetime_output = None
-    if isinstance(datetime_input, datetime):
-        datetime_output = datetime_input
-    elif isinstance(datetime_input, date):
-        datetime_output = datetime(datetime_input.year, datetime_input.month, datetime_input.day)
-    elif isinstance(datetime_input, int):
-        if datetime_input > 9999999999: datetime_input = datetime_input // 1000
-        datetime_output = datetime.fromtimestamp(datetime_input, tz=timezone.utc)
-    elif isinstance(datetime_input, str):
-        for fmt in ("%Y-%m-%d %H:%M:%S", "%Y/%m/%d %H:%M:%S", "%Y%m%d%H%M%S", "%Y-%m-%d", "%Y/%m/%d", "%Y%m%d"):
-            try:
-                datetime_output = datetime.strptime(datetime_input, fmt)
-                break
-            except ValueError:
-                continue
-    else:
-        raise TypeError(f"Unsupported datetime input type: {type(datetime_input)}")
-    if not isinstance(datetime_output, datetime): raise ValueError(f"String datetime format not recognized: {datetime_input}")
-    return datetime_output.astimezone()
 
 
 __all__ = [
