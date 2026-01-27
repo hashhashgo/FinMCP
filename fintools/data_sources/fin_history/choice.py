5801fafe026ec235582e094b98592091ae2067d8
hashhashgo <79754126+hashhashgo@users.noreply.github.com>
2026-01-27 09:39:44 +0800

    add release timer

--- fintools/data_sources/fin_history/choice.py
+++ fintools/data_sources/fin_history/choice.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 from typing import Optional, Callable, Union, Any
 from datetime import datetime, date, timedelta
-from threading import Lock
+from threading import Lock, Timer
 
 import importlib
 
@@ -13,7 +13,9 @@ class ChoiceDataSource(OHLCDataSource):
 
     name = "choice"
     choice: Any  # Placeholder for EmQuantAPI module
+    choice_running: bool = False
     lock: Lock
+    release_timer: Timer
 
     freq_map = {
         DataFrequency.DAILY: '1',
@@ -47,17 +49,33 @@ class ChoiceDataSource(OHLCDataSource):
         return self._format_dataframe(df)
 
     @classmethod
+    def _release(cls):
+        if hasattr(cls, 'choice'):
+            cls.choice.stop()
+            cls.choice_running = False
+            if cls.lock.locked(): cls.lock.release()
+            cls.release_timer.cancel()
+        
+    @classmethod
     @contextmanager
-    def borrow_choice(cls, timeout: float = -1):
+    def borrow_choice(cls, timeout: float = -1, release_after: float = 5.0):
         try:
             if not hasattr(cls, 'choice'):
                 cls.choice = importlib.import_module("EmQuantAPI").c
                 cls.lock = Lock()
         except (ModuleNotFoundError, AttributeError) as e:
             raise ImportError("EmQuantAPI module is not installed. Please install it to use ChoiceDataSource.") from e
+
+        if not cls.choice_running:
+            cls.choice_running = True
+            cls.choice.start(f"ForceLogin=1,UserName={os.getenv('CHOICE_USERNAME','')},Password={os.getenv('CHOICE_PASSWORD','')}")
+        if hasattr(cls, 'release_timer'):
+            cls.release_timer.cancel()
+        cls.release_timer = Timer(release_after, cls._release)
+        cls.release_timer.start()
+        
         try:
             if cls.lock.acquire(timeout=timeout):
-                cls.choice.start(f"ForceLogin=1,UserName={os.getenv('CHOICE_USERNAME','')},Password={os.getenv('CHOICE_PASSWORD','')}")
                 yield cls.choice
             else:
                 raise TimeoutError("Timeout while waiting to acquire Choice API lock")
@@ -65,7 +83,6 @@ class ChoiceDataSource(OHLCDataSource):
             raise e
         finally:
             cls.lock.release()
-            cls.choice.stop()
 
     def subscribe(self, symbol: str, interval: str, callback: Callable) -> None:
         raise NotImplementedError("Yahoo Finance does not support real-time data subscription")
